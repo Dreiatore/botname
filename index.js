ac08a98c83fec83ea6f037b27ee3a174732b92d2
Dreiatore <46677732+Dreiatore@users.noreply.github.com>
2019-01-14 22:05:50 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ and ping - to see how much ping you have`);
     let mute = message.mentions.members.first();
     let muteTime = message.content.split(' ').slice(2).join(' ')
     if (!mute || !muteTime) return message.reply("how to use: +mute <@user> <time>")
-    if (message.guild.member(mute).hasPermission('MANAGE_MESSAGES')) return message.reply('I cannot mute him, sorry');
+    if(message.guild.member(tomute).hasPermission('MANAGE_MESSAGES')) return message.reply('I cannot mute him, sorry');
 
     await message.guild.member(mute).addRole(muterole).then(() => {
         message.reply(`${mute.user.username} is muted for ${ms(ms(muteTime), {long: true})}.`);
